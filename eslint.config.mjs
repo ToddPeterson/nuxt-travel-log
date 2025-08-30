aa940847c2b8b5996acf3c3012003e11843a7384
Todd Peterson <ToddPetersonGV@gmail.com>
2025-08-30 14:36:33 -0600

    Add github action for linting on pull request (#48)
    * Add github action for linting on pull request

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -4,33 +4,34 @@ import antfu from '@antfu/eslint-config';
 import withNuxt from './.nuxt/eslint.config.mjs';
 
 export default withNuxt(
-   antfu(
-      {
-         type: 'app',
-         vue: true,
-         typescript: true,
-         formatters: true,
-         stylistic: {
-            indent: 3,
-            semi: true,
-         },
+  antfu(
+    {
+      type: 'app',
+      vue: true,
+      typescript: true,
+      formatters: true,
+      stylistic: {
+        indent: 2,
+        semi: true,
       },
-      {
-         rules: {
-            'perfectionist/sort-imports': [
-               'error',
-               {
-                  tsconfigRootDir: '.',
-               },
-            ],
-            'unicorn/filename-case': [
-               'error',
-               {
-                  case: 'kebabCase',
-                  ignore: ['README.md'],
-               },
-            ],
-         },
+      ignores: ['.pnpm-store/**'],
+    },
+    {
+      rules: {
+        'perfectionist/sort-imports': [
+          'error',
+          {
+            tsconfigRootDir: '.',
+          },
+        ],
+        'unicorn/filename-case': [
+          'error',
+          {
+            case: 'kebabCase',
+            ignore: ['README.md'],
+          },
+        ],
       },
-   ),
+    },
+  ),
 );
