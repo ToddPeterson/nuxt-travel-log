61c955d17634bb3e49845e6e31a180392aff68e5
Todd Peterson <ToddPetersonGV@gmail.com>
2025-08-31 17:10:57 -0600

    feature/17/auth-status (#56)
    * Show name and avatar when logged in
    * Implement sign-out view
    * fixes #17
    * fixes #18

---
+++ app/pages/sign-out.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+const authStore = useAuthStore();
+
+onMounted(() => {
+  authStore.signOut();
+});
+</script>
+
+<template>
+  <div class="card bg-base-300 container mt-4 mx-auto min-h-72 text-center flex flex-col items-center justify-center gap-4">
+    <span class="loading loading-spinner loading-xl" />
+  </div>
+</template>
