a69b34fea2d3f9451c8b4424f667df0e80d58ce9
Todd Peterson <tpeterson@artandlogic.com>
2025-09-01 09:21:50 -0600

    feature/20/ssr-auth-session
    * Provide session during SSR, not refetched on client
    * fixes #20

--- app/middleware/auth.global.ts
+++ app/middleware/auth.global.ts
@@ -3,10 +3,12 @@ import { createAuthClient } from 'better-auth/vue';
 const authClient = createAuthClient();
 
 export default defineNuxtRouteMiddleware(async (to) => {
+  if (!to.path.startsWith('/dashboard')) {
+    return;
+  }
+
   const { data: session } = await authClient.useSession(useFetch);
-  if (!session.value) {
-    if (to.path.startsWith('/dashboard')) {
-      return navigateTo('/');
-    }
+  if (!session.value?.user) {
+    return navigateTo('/');
   }
 });
