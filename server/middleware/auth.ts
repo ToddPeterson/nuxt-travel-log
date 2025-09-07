91aae07ffef652ddbe86467ae35eb7a22c87df03
Todd Peterson <ToddPetersonGV@gmail.com>
2025-09-07 09:14:43 -0600

    feature/22/create-locations (#60)
    * Implement POST /api/locations
    * Implement locations create form
    * fixes #22

---
+++ server/middleware/auth.ts
@@ -0,0 +1,9 @@
+import { auth } from '../auth';
+
+export default defineEventHandler(async (event) => {
+  const session = await auth.api.getSession({
+    headers: event.headers,
+  });
+
+  event.context.user = session?.user;
+});
